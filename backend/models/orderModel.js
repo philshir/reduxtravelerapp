c58266ffa353e9ce7ed0336f5bd13573992d8943
philshir <philshir77@gmail.com>
2020-11-11 15:04:49 -0600

    setup fetch products from database

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -73,6 +73,6 @@ const orderSchema = mongoose.Schema({
     timestamps: true
 })
 
-const User = mongoose.model('Order', orderSchema)
+const Order = mongoose.model('Order', orderSchema)
 
 export default Order
