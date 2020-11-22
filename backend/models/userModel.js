bbc729363e90f1fcb920c156cfd2a8343c916e13
philshir <philshir77@gmail.com>
2020-11-21 20:42:05 -0600

    added user auth routes and controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -27,6 +28,10 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
-const User = mongoose.model('User', userSchema)
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+const User = mongoose.model('User', userSchema) 
 
 export default User
