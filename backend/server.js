ad803bfc5f0d3983007935a85db2de9dae5adfdb
philshir <philshir77@gmail.com>
2020-12-11 03:22:32 -0600

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json(products)
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -36,7 +33,21 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')
+))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+})
+}
+
+
 
 app.use(notFound)
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { Image, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
@@ -17,11 +17,13 @@ const Header = () => {
   }
 
     return (
-        <header>
-            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+      <header>
+        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
           <Container>
             <LinkContainer to='/'>
-  <Navbar.Brand>ProShop</Navbar.Brand>
+              <Navbar.Brand>
+                Redux Traveler
+  </Navbar.Brand>
           </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
